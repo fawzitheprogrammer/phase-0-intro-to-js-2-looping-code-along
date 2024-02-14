8f15eddbe9ef5062d38b2b6f91ced9376e1609ed
Fawzi Gharib <fawz11gharib@gmail.com>
2024-02-14 14:59:49 +0300

    Assingment Completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// writeCards function
+function writeCards(names, event) {
+    const thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+  }
+  
+  // countDown function
+  function countDown(startNumber) {
+    while (startNumber >= 0) {
+      console.log(startNumber);
+      startNumber--;
+    }
+  }
+  
